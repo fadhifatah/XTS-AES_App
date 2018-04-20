29f61441e45f7431bf6841e1d0766009182f8882
Fatah Fadhlurrohman <fadhlurrohman.nf@gmail.com>
2018-04-20 20:35:47 +0700

    Complete XTS Encryption and Update Util

--- src/core/Util.java
+++ src/core/Util.java
@@ -8,7 +8,7 @@ import java.io.*;
  *
  *   @author Lawrie Brown, Oct 2001
  *
- *   Modification: add file2int and int2file functions
+ *   Modification: add multiplicationGF_128, file2int and int2file functions to help XTS
  *   Modified by Fatah F
  */
 class Util {
@@ -306,9 +306,20 @@ class Util {
         return(0);	// any other char is treated as 0
     }
 
+    public static final int KEY_SIZE = 256;
+    public static final int SPLIT_KEY_SIZE = 128;
+    public static final int BYTE_SIZE = 8;
+    public static final int BLOCK_SIZE = 128;
+    public static final int GF_SIZE = 128;
+    public static final int POLY_128 = 0x87;
+    public static final int ALPHA = 0x02;
+    public static final byte[] TWEAK = {0x00, 0x0f, 0x01, 0x0e,
+                                        0x02, 0x0d, 0x03, 0x0c,
+                                        0x04, 0x0b, 0x05, 0x0a,
+                                        0x06, 0x09, 0x07, 0x08};
+
     /**
      * Read file and then do conversion into int[]
-     * Originally created by prakash
      *
      * @param file
      * @return array of integer
@@ -337,7 +348,6 @@ class Util {
 
     /**
      * Write array of integer into file using FileOutputStream
-     * Originally created by prakash
      *
      * @param data
      * @param file
@@ -352,6 +362,61 @@ class Util {
 
         stream.write(dataInBytes);
         stream.close();
+    }
+
+    //multiplication in gf(2^128) with irreducible polynomial x^128 + x^7 + x^2 + x + 1
+    public static int[] multiplyGF2_128(int[] x, int[] y) {
+        if (x.length != BLOCK_SIZE / BYTE_SIZE || y.length != BLOCK_SIZE / BYTE_SIZE) {
+            return null;
+        }
+
+        int[][] arm = new int[GF_SIZE][y.length];
+        System.arraycopy(y, 0, arm[0], 0, y.length);
+
+        int[] temp = new int[y.length];
+        System.arraycopy(y, 0, temp, 0, y.length);
+
+        int n = 8;
+
+        for (int i = 1; i < GF_SIZE; i++) {
+            int m = (temp[0] & (1 << (n - 1))) >> (n - 1);
+            temp = shiftLeft(temp);
+            if (m == 1) {
+                temp[temp.length - 1] ^= POLY_128;
+            }
+            arm[i] = temp;
+        }
+
+        int[] res = new int[y.length];
+        for (int i = 0; i < GF_SIZE / BYTE_SIZE; i++) {
+            for (int j = 0; j < BYTE_SIZE; j++) {
+                if (((x[i] & (1 << j)) >> (j)) == 1) {
+                    for (int k = 0; k < res.length; k++) {
+                        res[k] = res[k] ^ arm[(GF_SIZE - (i + 1) * BYTE_SIZE) + j][k];
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private static int[] shiftLeft(int[] x) {
+        int msb = 0;
+        int n = 8;
+        int[] res = new int[x.length];
+
+        for (int i = x.length - 1; i >= 0; i--) {
+            int temp = (x[i] & (1 << (n - 1))) >> (n - 1);
+            res[i] = x[i] << 1;
+            res[i] &= ((1 << n) - 1);
+            if (msb == 1) {
+                res[i] |= 1;
+            }
+            msb = temp;
+        }
+
+        return res;
     }
 
 }
--- src/core/XTS.java
+++ src/core/XTS.java
@@ -2,31 +2,30 @@ package core;
 
 
 public class XTS {
-    private static final int KEY_SIZE = 256;
-    private static final int SPLIT_KEY_SIZE = 128;
-    private static final int BYTE_SIZE = 8;
-    private static final int BLOCK_SIZE = 128;
-
     private int[] key1;
     private int[] key2;
-    private AES aes1;
-    private AES aes2;
+    private int[] alpha;
+    private static AES aes1;
+    private static AES aes2;
 
     public XTS() {
-        this.key1 = new int[SPLIT_KEY_SIZE / BYTE_SIZE];
-        this.key2 = new int[SPLIT_KEY_SIZE / BYTE_SIZE];
+        this.key1 = new int[Util.SPLIT_KEY_SIZE / Util.BYTE_SIZE];
+        this.key2 = new int[Util.SPLIT_KEY_SIZE / Util.BYTE_SIZE];
+        this.alpha = new int[Util.SPLIT_KEY_SIZE / Util.BYTE_SIZE];
 
-        this.aes1 = new AES();
-        this.aes2 = new AES();
+        this.alpha[this.alpha.length - 1] = Util.ALPHA;
+
+        aes1 = new AES();
+        aes2 = new AES();
     }
 
     public int[] encrpyt(int[] key, int[] plainText) throws Exception{
         // set-up needed keys
-        if (key.length != KEY_SIZE / BYTE_SIZE)
+        if (key.length != Util.KEY_SIZE / Util.BYTE_SIZE)
             throw new Exception("Size of key is must be 256 bits!");
 
         System.arraycopy(key, 0, key1, 0, key.length);
-        System.arraycopy(key, SPLIT_KEY_SIZE / BYTE_SIZE, key2, 0, key.length);
+        System.arraycopy(key, Util.SPLIT_KEY_SIZE / Util.BYTE_SIZE, key2, 0, key.length);
 
         aes1.setKey(Util.int2byte(key1));
         aes2.setKey(Util.int2byte(key2));
@@ -34,20 +33,68 @@ public class XTS {
         // initiate return cipher-text object
         int[] cipherText = new int[plainText.length];
 
-        int blockIteration = plainText.length / (BLOCK_SIZE / BYTE_SIZE);
-        int[] plainTextPerBlock = new int[BLOCK_SIZE / BYTE_SIZE];
+        int blockIteration = plainText.length / (Util.BLOCK_SIZE / Util.BYTE_SIZE);
+        int[] plainTextPerBlock = new int[Util.BLOCK_SIZE / Util.BYTE_SIZE];
         int[] cipherTextPerBlock;
 
         // begin encryption
         for (int i = 0; i < blockIteration - 1; i++) {
             System.arraycopy(plainText, i * 16, plainTextPerBlock, 0, plainTextPerBlock.length);
             cipherTextPerBlock = blockEncryption(plainTextPerBlock, i);
+
+            System.arraycopy(cipherTextPerBlock, 0, cipherText, i * 16, cipherTextPerBlock.length);
+        }
+
+        // handling for the last block
+        if (plainText.length % (Util.BLOCK_SIZE / Util.BYTE_SIZE) == 0) {
+            System.arraycopy(plainText, (blockIteration - 1) * 16, plainTextPerBlock, 0, plainTextPerBlock.length);
+            cipherTextPerBlock = blockEncryption(plainTextPerBlock, blockIteration - 1);
+
+            System.arraycopy(cipherTextPerBlock, 0, cipherText, (blockIteration - 1) * 16, cipherTextPerBlock.length);
+        }
+        // if the last block size isn't 128 bit then do stealing
+        else {
+            int lastBlockLength = plainText.length - (blockIteration * 16);
+            int[] lastPlainTextBlock = new int[16];
+
+            System.arraycopy(plainText, (blockIteration * 16), lastPlainTextBlock, 0, lastBlockLength);
+
+            // stealing
+            System.arraycopy(plainText, (blockIteration - 1) * 16, plainTextPerBlock,0, plainTextPerBlock.length);
+            int[] cipherTextBlockNmin1 = blockEncryption(plainTextPerBlock, blockIteration - 1);
+
+            System.arraycopy(cipherTextBlockNmin1, 0, cipherText, blockIteration * 16, lastBlockLength);
+
+            System.arraycopy(cipherTextBlockNmin1, lastBlockLength, lastPlainTextBlock, lastBlockLength, lastPlainTextBlock.length - lastBlockLength);
+            cipherTextPerBlock = blockEncryption(lastPlainTextBlock, blockIteration);
+
+            System.arraycopy(cipherTextPerBlock, 0, cipherText, (blockIteration - 1) * 16, cipherTextPerBlock.length);
         }
 
         return cipherText;
     }
 
-    private int[] blockEncryption(int[] plainTextPerBlock, int i) {
-        return null;
+    private int[] blockEncryption(int[] plainTextPerBlock, int blockIndex) {
+        int[] firstAESEncryption = Util.byte2int(aes2.encrypt(Util.TWEAK));
+
+        int[] tmp = this.alpha;
+        for (int i = 0; i < blockIndex - 1; i++) {
+            assert tmp != null;
+            tmp = Util.multiplyGF2_128(tmp, this.alpha);
+        }
+
+        int[] tmp2 = Util.multiplyGF2_128(firstAESEncryption, tmp);
+
+        int[] tmp3 = new int[Util.BLOCK_SIZE / Util.BYTE_SIZE];
+        for (int j = 0; j < tmp3.length; j++)
+            tmp3[j] = plainTextPerBlock[j] ^ tmp2[j];
+
+        int[] secondAESEncryption = Util.byte2int(aes1.encrypt(Util.int2byte(tmp3)));
+
+        int[] result = new int[Util.BLOCK_SIZE / Util.BYTE_SIZE];
+        for (int k = 0; k < result.length; k++)
+            result[k] = secondAESEncryption[k] ^ tmp2[k];
+
+        return result;
     }
 }
